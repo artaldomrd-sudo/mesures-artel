afb914732c4d2d93b36137d5299928567daa587e
Dylan Robert <dylanrobert@MacBook-Pro-de-Dylan.local>
2026-07-27 10:29:15 -0400

    Sitio web: seccion Historias destacadas (circulos + visor tipo stories)

---
+++ sitio-web/historias-data.js
@@ -0,0 +1,88 @@
+window.HISTORIAS=[
+{
+"slug": "obra-en-proceso",
+"titulo": "Obra en proceso",
+"emoji": "🏗️",
+"cover": "historias/obra-en-proceso/cover.jpg",
+"n": 3,
+"slides": [
+{
+"tipo": "img",
+"src": "historias/obra-en-proceso/01.jpg",
+"cap": "Avance de obra"
+},
+{
+"tipo": "img",
+"src": "historias/obra-en-proceso/02.jpg",
+"cap": "Fachada en montaje"
+},
+{
+"tipo": "img",
+"src": "historias/obra-en-proceso/03.jpg",
+"cap": "Instalacion de corredera E100"
+}
+]
+},
+{
+"slug": "ofertas",
+"titulo": "Ofertas",
+"emoji": "🏷️",
+"cover": "historias/ofertas/cover.jpg",
+"n": 2,
+"slides": [
+{
+"tipo": "img",
+"src": "historias/ofertas/01.jpg",
+"cap": "Consulta nuestras promociones"
+},
+{
+"tipo": "img",
+"src": "historias/ofertas/02.jpg",
+"cap": "Vidrio de ducha a la medida"
+}
+]
+},
+{
+"slug": "novedades",
+"titulo": "Novedades",
+"emoji": "📢",
+"cover": "historias/novedades/cover.jpg",
+"n": 2,
+"slides": [
+{
+"tipo": "img",
+"src": "historias/novedades/01.jpg",
+"cap": "Cotiza sin compromiso"
+},
+{
+"tipo": "img",
+"src": "historias/novedades/02.jpg",
+"cap": "Nuevo sitio web en linea"
+}
+]
+},
+{
+"slug": "terminadas",
+"titulo": "Terminadas",
+"emoji": "✅",
+"cover": "historias/terminadas/cover.jpg",
+"n": 3,
+"slides": [
+{
+"tipo": "img",
+"src": "historias/terminadas/01.jpg",
+"cap": "Villa frente al mar"
+},
+{
+"tipo": "img",
+"src": "historias/terminadas/02.jpg",
+"cap": "Bano terminado"
+},
+{
+"tipo": "img",
+"src": "historias/terminadas/03.jpg",
+"cap": "Baranda de vidrio"
+}
+]
+}
+];
